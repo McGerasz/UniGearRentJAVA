841b513bdcd35844244c00a4aaadb1df71c3a49f
McGerasz <mcgerasz@gmail.com>
2024-03-11 13:21:32 +0100

    git init

---
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/UniGearRentJavaApplication.java
@@ -0,0 +1,13 @@
+package unigearrent.unigearrentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniGearRentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniGearRentJavaApplication.class, args);
+    }
+
+}
---
+++ UniGearRentJAVA/src/test/java/unigearrent/unigearrentjava/UniGearRentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package unigearrent.unigearrentjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniGearRentJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
