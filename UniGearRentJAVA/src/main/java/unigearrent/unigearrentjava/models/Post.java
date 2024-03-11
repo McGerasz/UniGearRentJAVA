176518058907040dea810b035bae437d4263d1ea
McGerasz <mcgerasz@gmail.com>
2024-03-11 13:29:38 +0100

    feat: add Post class

---
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/models/Post.java
@@ -0,0 +1,24 @@
+package unigearrent.unigearrentjava.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public abstract class Post {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String Name;
+    private String Location;
+    private String PosterId;
+    private String Descrpition;
+    private Integer HourlyPrice;
+    private Integer DailyPrice;
+    private Integer SecurityDeposit;
+}
