63846bc1f9c2fa254952b2afd3ff0ed6bf971f8b
McGerasz <mcgerasz@gmail.com>
2024-03-19 10:28:50 +0100

    feat: add RSAKeys utility class

---
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/services/miscellaneous/RSAKeys.java
@@ -0,0 +1,24 @@
+package unigearrent.unigearrentjava.services.miscellaneous;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
+@Getter
+@Setter
+public class RSAKeys {
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    public RSAKeys()
+    {
+        KeyPair pair = KeyGenerator.GenerateRsaKey();
+        this.publicKey = (RSAPublicKey) pair.getPublic();
+        this.privateKey = (RSAPrivateKey) pair.getPrivate();
+    }
+}
