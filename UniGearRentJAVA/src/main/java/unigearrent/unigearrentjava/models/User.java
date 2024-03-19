e370cd1bd44d89612391ab80c33afc527d33247e
McGerasz <mcgerasz@gmail.com>
2024-03-19 10:26:57 +0100

    feat: add User class

---
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/models/User.java
@@ -0,0 +1,116 @@
+package unigearrent.unigearrentjava.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Builder
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+
+    private String email;
+
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+
+    @JsonIgnore
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role_junction",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> authorities;
+
+
+    public Integer getUserId()
+    {
+        return this.id;
+    }
+
+    public void setUserId(Integer id)
+    {
+        this.id = id;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities)
+    {
+        this.authorities = authorities;
+    }
+
+
+    @Override
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
