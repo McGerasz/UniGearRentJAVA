a6174618c984309b42cc25c7f400f3d0a34f5327
McGerasz <mcgerasz@gmail.com>
2024-03-19 10:26:15 +0100

    feat: add ILessorDetailsRepository

---
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/repositories/ILessorDetailsRepository.java
@@ -0,0 +1,7 @@
+package unigearrent.unigearrentjava.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import unigearrent.unigearrentjava.models.LessorDetails;
+
+public interface ILessorDetailsRepository extends JpaRepository<LessorDetails, Integer> {
+}
