377981193d19290af173e89aa9bba8c7f85ae513
McGerasz <mcgerasz@gmail.com>
2024-03-20 12:30:10 +0100

    fix: add missing lines to ILessorDetailsRepository

--- UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/repositories/ILessorDetailsRepository.java
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/repositories/ILessorDetailsRepository.java
@@ -1,7 +1,9 @@
 package unigearrent.unigearrentjava.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unigearrent.unigearrentjava.models.LessorDetails;
 
+@Repository
 public interface ILessorDetailsRepository extends JpaRepository<LessorDetails, Integer> {
 }
