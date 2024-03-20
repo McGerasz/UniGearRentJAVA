43b8ca4e49dbe5718342df05f674a61d3f5d03e8
McGerasz <mcgerasz@gmail.com>
2024-03-20 12:29:08 +0100

    fix: fix an error in SecurityConfiguration

--- UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/configuration/SecurityConfiguration.java
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/configuration/SecurityConfiguration.java
@@ -55,14 +55,7 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/uploads/avatars/**").permitAll();
-                    auth.requestMatchers("/auth/**").permitAll();
-                    auth.requestMatchers("/admin/**").hasRole("ADMIN");
-                    auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
-                    auth.requestMatchers("/company/**").hasAnyRole("ADMIN", "COMPANY");
-                    auth.requestMatchers("/appointment/**").hasAnyRole("ADMIN", "USER", "COMPANY");
-
-
+                    auth.requestMatchers("/api/Auth/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(oauth2 -> oauth2
