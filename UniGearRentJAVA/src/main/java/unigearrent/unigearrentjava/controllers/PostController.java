4c3dfd024c264502eca050ff957ca70a186955e0
McGerasz <mcgerasz@gmail.com>
2024-04-03 11:42:29 +0200

    feat: add PostController

---
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/controllers/PostController.java
@@ -0,0 +1,45 @@
+package unigearrent.unigearrentjava.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import unigearrent.unigearrentjava.models.LessorDetails;
+import unigearrent.unigearrentjava.services.repositoryservices.CarService;
+import unigearrent.unigearrentjava.services.repositoryservices.LessorDetailsService;
+import unigearrent.unigearrentjava.services.repositoryservices.TrailerService;
+import unigearrent.unigearrentjava.services.repositoryservices.UserService;
+
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/Post")
+public class PostController {
+    @Autowired
+    private CarService carService;
+    @Autowired
+    private TrailerService trailerService;
+    @Autowired
+    private LessorDetailsService lessorDetailsService;
+    @GetMapping("/byName/{name}")
+    public ResponseEntity<?> GetByName(@PathVariable String name){
+        List<Object> postList = new ArrayList<>();
+        List<LessorDetails> details = lessorDetailsService.GetContainingName(name);
+        if((long) details.size() == 0) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No matching element was found");
+        details.forEach(detail -> postList.addAll(detail.getPosts()));
+        System.out.println(details.get(0).getPosts());
+        return ResponseEntity.status(HttpStatus.OK).body(postList);
+    }
+    @GetMapping("/byLocation/{location}")
+    public ResponseEntity<?> GetByLocation(@PathVariable String location){
+        List<Object> postList = new ArrayList<>(carService.GetAll().stream().filter(post -> post.getLocation().toLowerCase().contains(location.toLowerCase())).toList());
+        postList.addAll(trailerService.GetAll().stream().filter(trailerPost -> trailerPost.getLocation().toLowerCase().contains(location.toLowerCase())).toList());
+        return ResponseEntity.status(HttpStatus.OK).body(postList);
+
+    }
+}
