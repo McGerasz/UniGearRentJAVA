8c08b123427bc58930e5bb61dbeffa3e6f24381d
McGerasz <mcgerasz@gmail.com>
2024-04-11 10:43:37 +0200

    feat: add basic favourites endpoints

--- UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/controllers/PostController.java
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/controllers/PostController.java
@@ -5,14 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import unigearrent.unigearrentjava.models.CarPost;
-import unigearrent.unigearrentjava.models.LessorDetails;
-import unigearrent.unigearrentjava.models.Post;
-import unigearrent.unigearrentjava.models.TrailerPost;
-import unigearrent.unigearrentjava.services.repositoryservices.CarService;
-import unigearrent.unigearrentjava.services.repositoryservices.LessorDetailsService;
-import unigearrent.unigearrentjava.services.repositoryservices.TrailerService;
-import unigearrent.unigearrentjava.services.repositoryservices.UserService;
+import unigearrent.unigearrentjava.models.*;
+import unigearrent.unigearrentjava.services.repositoryservices.*;
 
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
@@ -30,6 +24,8 @@ public class PostController {
     private TrailerService trailerService;
     @Autowired
     private LessorDetailsService lessorDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Autowired
     private UserService userService;
     @GetMapping("/byName/{name}")
@@ -85,5 +81,45 @@ public class PostController {
             }
         }
         return ResponseEntity.status(HttpStatus.OK).body(lessorDetailsService.GetById(posterId));
+    }
+    @PostMapping("/favourite")
+    public ResponseEntity<?> PostFavourite(@RequestParam String userName, @RequestParam Integer id){
+        Integer userId = 0;
+        try {
+            userId = userService.getUserByUsername(userName).getId();
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The user was not found");
+        }
+        UserDetails details = userDetailsService.GetById(userId);
+        List<Post> favourites = details.getFavouriteIDs();
+        Optional<CarPost> carPost = carService.GetById(id);
+        if(carPost.isPresent()){
+            favourites.add(carPost.get());
+        }
+        else {
+            Optional<TrailerPost> trailerPost = trailerService.GetById(id);
+            if (trailerPost.isPresent()) {
+                favourites.add(trailerPost.get());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No post with the id " + id.toString() + " was found");
+            }
+        }
+        userDetailsService.Update(details);
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+    @GetMapping("getFavourites/{userName}")
+    public ResponseEntity<?> GetFavourites(@PathVariable String userName){
+        Integer userId = 0;
+        try {
+            userId = userService.getUserByUsername(userName).getId();
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The user was not found");
+        }
+        UserDetails details = userDetailsService.GetById(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(details.getFavouriteIDs());
     }
 }
