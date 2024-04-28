87cc09fc45070f2bd306d7583e11d08d5aa5b5b2
McGerasz <mcgerasz@gmail.com>
2024-04-28 15:47:21 +0200

    feat: add JsonProperties to new DTOs

--- UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/models/LessorPutRequestDTO.java
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/models/LessorPutRequestDTO.java
@@ -1,5 +1,6 @@
 package unigearrent.unigearrentjava.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class LessorPutRequestDTO {
+    @JsonProperty("id")
     private Integer Id;
+    @JsonProperty("username")
     private String Username;
+    @JsonProperty("email")
     private String Email;
+    @JsonProperty("phonenumber")
     private String Phonenumber;
+    @JsonProperty("name")
     private String Name;
 }
--- UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/models/UserPutRequestDTO.java
+++ UniGearRentJAVA/src/main/java/unigearrent/unigearrentjava/models/UserPutRequestDTO.java
@@ -1,5 +1,6 @@
 package unigearrent.unigearrentjava.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class UserPutRequestDTO {
+    @JsonProperty("id")
     private Integer Id;
+    @JsonProperty("username")
     private String Username;
+    @JsonProperty("email")
     private String Email;
+    @JsonProperty("phonenumber")
     private String Phonenumber;
+    @JsonProperty("firstName")
     private String FirstName;
+    @JsonProperty("lastName")
     private String LastName;
 }
